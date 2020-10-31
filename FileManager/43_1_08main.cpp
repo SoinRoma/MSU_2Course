b3c712d97fd7e6e306b543bd3b98ce07098b0d89
SoinRoma <61582942+SoinRoma@users.noreply.github.com>
2020-11-01 00:07:25 +0500

    Add files via upload

---
+++ FileManager/43_1_08main.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include "os_file.h"
+using namespace std;
+int main()
+{
+    file_manager_t fm;
+    setup_file_manager(&fm);
+   /* int result;
+    result = fm.create(1000);printf("1expected: 1 = %d\n", result);// expected: 1, passed
+    result = fm.create_dir("dir1");printf("1expected: 1 = %d\n", result);// expected: 1, passed
+    result = fm.create_dir("dir1/dir11");printf("1expected: 1 = %d\n", result);// expected: 1, passed
+    result = fm.change_dir("dir1");printf("1expected: 1 = %d\n", result);// expected: 1, passed
+    result = fm.create_dir("../dir2");printf("1expected: 1 = %d\n", result);// expected: 1, passed
+    result = fm.create_dir("../dir2/dir3");printf("1expected: 1 = %d\n", result);// expected: 1, passed
+    result = fm.remove("/dir2/dir3", 0);printf("1expected: 1 = %d\n", result);// expected: 1, passed
+    result = fm.create_dir("/dir3/dir31");printf("1expected: 0 = %d\n", result);// expected: 0, passed
+    result = fm.create_dir("../dir3/dir31");printf("1expected: 0 = %d\n", result);// expected: 0, passed
+    result = fm.create_dir("../dir2");printf("1expected: 0 = %d\n", result);// expected: 0, passed
+    result = fm.create_file("/dir2/file1", 1);printf("1expected: 1 = %d\n", result);// expected: 1, passed
+    result = fm.create_dir("/dir2/dir21");printf("1expected: 1 = %d\n", result);// expected: 1, passed
+    result = fm.create_dir("/dir2/file1");printf("1expected: 0 = %d\n", result);// expected: 0, passed
+    result = fm.create_dir("../dir2/file1");printf("1expected: 0 = %d\n", result);// expected: 0, passed
+    result = fm.create_dir("../dir2/file1/dir");printf("1expected: 0 = %d\n", result);// expected: 0, passed
+    result = fm.create_dir("../dir2/dir22");printf("1expected: 1 = %d\n", result);// expected: 1, passed
+    result = fm.change_dir("dir2");printf("1expected: 0 = %d\n", result);// expected: 0, passed
+    result = fm.change_dir("dir11");printf("1expected: 1 = %d\n", result);// expected: 1, passed
+    result = fm.remove("../../dir2/file1", 0);printf("1expected: 1 = %d\n", result);// expected: 1, passed
+    result = fm.create_dir("../../dir2/file1");printf("1expected: 1 = %d\n", result);// expected: 1, passed
+    result = fm.remove("../../dir2/file1", 0);printf("1expected: 1 = %d\n", result);// expected: 1, passed
+    result = fm.create_file("../../dir2/file1", 1);printf("1expected: 1 = %d\n", result);// expected: 1, passed
+    result = fm.change_dir(".");printf("1expected: 1 = %d\n", result);// expected: 1, passed
+    result = fm.change_dir("/dir1/dir11");printf("1expected: 1 = %d\n", result);// expected: 1, passed
+    result = fm.change_dir("/dir1/dir11/dir3");printf("1expected: 0 = %d\n", result);// expected: 0, passed
+    result = fm.change_dir("/dir1");printf("1expected: 1 = %d\n", result);// expected: 1, passed
+    result = fm.change_dir("./dir11");printf("1expected: 1 = %d\n", result);// expected: 1, passed
+    result = fm.change_dir("..");printf("1expected: 1 = %d\n", result);// expected: 1, passed
+    result = fm.create_file("./dir11/file.txt", 1);printf("1expected: 1 = %d\n", result);// expected: 1, passed
+    result = fm.remove("dir11", 0);printf("1expected: 0 = %d\n", result);// expected: 0, passed
+    result = fm.remove("./dir11", 0);printf("1expected: 0 = %d\n", result);// expected: 0, passed
+    result = fm.remove("./dir11", 1);printf("1expected: 1 = %d\n", result);// expected: 1, passed
+    result = fm.create_file("file11.txt",1);printf("1expected: 1 = %d\n", result);// expected: 1, passed
+    result = fm.create_file("../dir2/a_file22.txt",1);printf("1expected: 1 = %d\n", result);// expected: 1, passed
+    result = fm.copy("../dir2/a_file22.txt","a_file22.txt");printf("1expected: 1 = %d\n", result);// expected: 1, passed
+    result = fm.create_dir("a_file22.txt");printf("1expected: 0 = %d\n", result);// expected: 0, passed
+    result = fm.create_dir("./a_file22.txt");printf("1expected: 0 = %d\n", result);// expected: 0, passed
+    result = fm.create_dir("a_file22.txt/1");printf("1expected: 0 = %d\n", result);// expected: 0, passed
+    result = fm.copy("../dir2", "./dir2");printf("1expected: 2 = %d\n", result);// expected: 2, failed
+    printf("15 =%d\n", result);*/
+    return 0;
+}
