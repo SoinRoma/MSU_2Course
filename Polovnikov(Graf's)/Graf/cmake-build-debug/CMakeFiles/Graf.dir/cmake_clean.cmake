0037327ef474c3385144fe96049f7736c1f7bd99
SoinRoma <61582942+SoinRoma@users.noreply.github.com>
2020-11-01 00:12:32 +0500

    Add files via upload

---
+++ Polovnikov(Graf's)/Graf/cmake-build-debug/CMakeFiles/Graf.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Graf.dir/main.cpp.obj"
+  "Graf.pdb"
+  "Graf.exe"
+  "Graf.exe.manifest"
+  "libGraf.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Graf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Polovnikov(Graf's)/Graf2/cmake-build-debug/CMakeFiles/Graf2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Graf2.dir/main.cpp.obj"
+  "Graf2.pdb"
+  "Graf2.exe"
+  "Graf2.exe.manifest"
+  "libGraf2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Graf2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
