54beee7a18ea43dfe051e8f108140476c0cd198b
SoinRoma <61582942+SoinRoma@users.noreply.github.com>
2020-11-01 00:17:01 +0500

    Add files via upload

---
+++ MatricaDerevo/cmake-build-debug/CMakeFiles/MatricaDerevo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MatricaDerevo.dir/main.cpp.obj"
+  "MatricaDerevo.exe"
+  "MatricaDerevo.exe.manifest"
+  "MatricaDerevo.pdb"
+  "libMatricaDerevo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MatricaDerevo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
