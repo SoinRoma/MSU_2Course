92d4ec225f231b903ee340fdfa46d545a119dfa4
SoinRoma <61582942+SoinRoma@users.noreply.github.com>
2020-11-01 00:19:48 +0500

    Add files via upload

---
+++ MinimalnoeOstovnoeDerevo/cmake-build-debug/CMakeFiles/MinimalnoeOstovnoeDerevo.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MinimalnoeOstovnoeDerevo.dir/Algorithm.cpp.obj"
+  "CMakeFiles/MinimalnoeOstovnoeDerevo.dir/Function.cpp.obj"
+  "CMakeFiles/MinimalnoeOstovnoeDerevo.dir/Interface.cpp.obj"
+  "CMakeFiles/MinimalnoeOstovnoeDerevo.dir/Test.cpp.obj"
+  "CMakeFiles/MinimalnoeOstovnoeDerevo.dir/main.cpp.obj"
+  "MinimalnoeOstovnoeDerevo.exe"
+  "MinimalnoeOstovnoeDerevo.exe.manifest"
+  "MinimalnoeOstovnoeDerevo.pdb"
+  "libMinimalnoeOstovnoeDerevo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MinimalnoeOstovnoeDerevo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
